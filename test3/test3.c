e28566a5c4ec6a875b43723c8c181270bfd0d24b
mountain-mountain-mountain <2374798912@qq.com>
2022-04-25 15:49:24 +0800

    平方数

---
+++ test3/test3.c
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+//题目：一个整数，它加上100后是一个完全平方数，再加上168又是一个完全平方数，请问该数是多少？
+
+
+
+#include<stdio.h>
+
+int main()
+{
+	int x, m, n, i, j;
+	for (i = 1; i < 168 / 2 + 1; i++)
+	{
+		if (168% i == 0)
+		{
+			j = 168 / i;
+			if (i > j && (i + j) % 2 == 0 && (i - j) % 2 == 0)
+			{
+				m = (i + j) / 2;
+				n = (i - j) / 2;
+				x = n * n - 100;
+				printf("x = %d\n",x);
+			}
+		}
+	}
+
+	return 0;
+}
