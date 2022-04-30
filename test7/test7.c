529c9dfa3f4d3940d8d9ea7f4ff3ce54c17a0a12
mountain-mountain-mountain <2374798912@qq.com>
2022-05-01 00:43:08 +0800

    测试

--- test7/test7.c
+++ test7/test7.c
@@ -1,14 +1,23 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
 
-
-int main()
+int main(void)
 {
-	char a = 176;
-	char b = 219;
-	printf("%c%c%c%c\n", a, a, b, b);
-
-
-return 0;
+    int i, j;
+    SetConsoleOutputCP(437);
+    char a = 176, b = 219;
+    for (i = 0; i < 5; i++)
+    {
+        for (j = 0; j < 5; j++)
+        {
+            if (i == j || (i + j + 1) == 5)
+                printf("%c", b);
+            else
+                printf("%c%c", a, a);
+        }
+        printf("\n");
+    }
 }
