2d3feb35ea9e4ae46bbb2ef094dbc24a5f3aa2d3
mountain-mountain-mountain <2374798912@qq.com>
2022-05-01 16:18:55 +0800

    楼梯

---
+++ test10/test.c
@@ -0,0 +1,20 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+//题目：打印楼梯，同时在楼梯上方打印两个笑脸。
+#include<stdio.h>
+
+
+
+int main()
+{
+	printf("◎◎\n"); /*输出两个笑脸*/
+	for (int a = 0; a < 10; a++)
+	{
+		for (int b = 0; b < a + 1; b++)
+		{
+			printf("■");
+         }
+		printf("\n");
+	}
+	
+return 0;
+}
