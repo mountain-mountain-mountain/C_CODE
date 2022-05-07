ac10e36fcfdb73fb8fc8ac2fef4aa274e985035f
mountain-mountain-mountain <2374798912@qq.com>
2022-05-07 16:06:39 +0800

    s=a+aa+aaa+...

---
+++ test18/test.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//题目：求s = a + aa + aaa + aaaa + aa...a的值，其中a是一个数字。
+//例如2 + 22 + 222 + 2222 + 22222(此时共有5个数相加)，几个数相加有键盘控制。
+#include<stdio.h>
+
+
+int main()
+{
+	int s = 0;
+	int  a, n, t;
+	scanf("%d%d", &a, &n);
+	t = a;
+	while (n > 0)
+	{
+		s += t;
+		a = a * 10;
+		t += a;
+		n--;
+	}
+	printf("s=a+aa+aaa+...=%d\n", s);
+return 0;
+}
