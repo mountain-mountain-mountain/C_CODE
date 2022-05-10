c44aaac61a96366353e819c7e0c09904319a8a00
mountain-mountain-mountain <2374798912@qq.com>
2022-05-10 16:27:17 +0800

    匹配对手

---
+++ test22/test.c
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//题目：两个乒乓球队进行比赛，各出三人。甲队为a, b, c三人，乙队为x, y, z三人。已抽签决定比赛名单。
+//有人向队员打听比赛的名单。a说他不和x比，c说他不和x, z比，请编程序找出三队赛手的名单。
+#include<stdio.h>
+
+
+int main()   
+{
+    char a, b, c;
+    for (a = 'X'; a <= 'Z'; a++)
+    {
+        for (b = 'X'; b <= 'Z'; b++)
+        {
+            if (a != b)
+            {
+                for (c = 'X'; c <= 'Z'; c++)
+                {
+                    if (a != c && b != c)
+                    {
+                        if (a != 'X' && c != 'X' && c != 'Z')
+                        {
+                            printf("a--%c,b--%c,c--%c\n", a, b, c);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+return 0;
+}
