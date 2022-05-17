cfdac8c4909bdb0d26eefbcd7ef84fd0143f5daa
mountain-mountain-mountain <2374798912@qq.com>
2022-05-17 21:12:25 +0800

    给一个不多于5位的正整数，要求：一、求它是几位数，二、逆序打印出各位数字。

---
+++ test29/test.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//题目：给一个不多于5位的正整数，要求：一、求它是几位数，二、逆序打印出各位数字。
+#include<stdio.h>
+
+
+static char a[5];
+void f(int n)
+{
+    while (n > 0)
+    {
+        printf("%c", a[--n]);
+    }
+}
+int main()
+{
+	printf("请输入整数：");
+	scanf("%s", a);
+	printf("这是一个%lu 位数\n", strlen(a));
+	f(strlen(a));
+
+
+
+return 0;
+}
+
+
