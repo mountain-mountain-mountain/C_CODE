70f7d0e4748ad14b78c8e2324d8840defd33c2bd
mountain-mountain-mountain <2374798912@qq.com>
2022-05-23 19:24:39 +0800

    宏定义

---
+++ test46/test.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//ÌâÄ¿£ººê#defineÃüÁîÁ·Ï°¡£
+#include<stdio.h>
+#define pai 3.1415926
+
+int main()
+{
+	float s;
+	float r = 2;
+	s = pai * r * r;
+	printf("%f",s);
+
+return 0;
+}
