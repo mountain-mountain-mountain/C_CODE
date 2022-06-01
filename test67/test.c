3f276719723cf001aa795c562ca7440a984d8b18
mountain-mountain-mountain <2374798912@qq.com>
2022-06-02 00:08:36 +0800

    交换

---
+++ test67/test.c
@@ -0,0 +1,54 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//题目：输入数组，最大的与第一个元素交换，最小的与最后一个元素交换，输出数组。
+#include<stdio.h>
+void jh(int b[])
+{
+	int max, min;
+	for (int j = 1; j < 5; j++)
+	{
+		if (b[0] > b[j])
+		{
+			max = b[0];
+		}
+		else
+		{
+			int t;
+			max = b[j];
+			t = b[0];
+			b[0] = max;
+			b[j] = t;
+		}
+		if (b[4] < b[j])
+		{
+			min = b[4];
+		}
+		else
+		{
+			int t;
+			min = b[j];
+			t = b[4];
+			b[4] = min;
+			b[j] = t;
+		}
+	}
+	
+}
+
+int main()
+{
+	int a[5];
+	int* p;
+	printf("请输入五个整数:\n");
+	for (int i = 0; i < 5; i++) 
+	{
+		scanf("%d", &a[i]);
+	}
+	jh(a);
+	for (p = a; p < a + 5; p++)    //p指针变量
+	{
+		printf("%-5d", *p);
+	}
+
+
+return 0;
+}
